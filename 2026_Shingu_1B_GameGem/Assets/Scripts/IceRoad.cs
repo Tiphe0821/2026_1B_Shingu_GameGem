4808cae24e3bbfa3e1724e844c79e83b5d7a5bdb
Tiphe0821 <namucho24@gmail.com>
2026-05-14 16:08:41 +0900

    [feat] 플레이어 개선 & 미니게임용 플레이어 컨트롤 업데이트

---
+++ 2026_Shingu_1B_GameGem/Assets/Scripts/IceRoad.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class IceRoad : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- 2026_Shingu_1B_GameGem/Assets/Scripts/PlayerController.cs
+++ 2026_Shingu_1B_GameGem/Assets/Scripts/PlayerController.cs
@@ -9,8 +9,9 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveInput;
     public float moveSpeed = 2;     // 나중에 SO 사용으로 조절 가능하도록 할 예정
 
-    public bool isCanMove;      // 움직일 수 있는가?
-    public bool isMiniGame;     // 미니게임 진행 중인지 확인
+    public bool isMiniGame = false;     // 미니게임 진행 중인지 확인 
+    public bool miniCanMove = true;      // 미니게임 진행 중 움직일 수 있는가?
+    private Vector2 miniMoveInput;
 
     [Header ("애니메이션 관련 데이터")]
     public int side = 4;            // 캐릭터가 보는 방향 (4가 기본인 이유 : 캐릭터의 얼굴이 온전히 보이는 앞면이기 때문)
@@ -59,17 +60,58 @@ public class PlayerController : MonoBehaviour
         return currentSpeed;
     }
 
+    public bool MiniCanMove
+    {
+        get { return miniCanMove; }
+        set { miniCanMove = value; }
+    }
+
+    public bool IsMiniGame
+    {
+        get { return isMiniGame; }
+        set { isMiniGame = value; }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("IceWall"))
+        {
+            miniCanMove = true;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(moveInput.normalized.x * moveSpeed * Time.deltaTime, moveInput.normalized.y * moveSpeed * Time.deltaTime, 1.0f);
-
+        if (!isMiniGame)
+        {
+            transform.Translate(moveInput.normalized.x * moveSpeed * Time.deltaTime, moveInput.normalized.y * moveSpeed * Time.deltaTime, 1.0f);
+        }
+        else
+        {
+            if(MiniCanMove)
+            {
+                if(moveInput.x != 0)
+                {
+                    miniMoveInput.x = moveInput.y;
+                    miniCanMove = false;
+                }
+                else if(moveInput.y != 0)
+                {
+                    miniMoveInput.y = moveInput.y;
+                    miniCanMove = false;
+                }
+            }
+            else
+            {
+                transform.Translate(miniMoveInput.normalized.x * moveSpeed * 1.2f * Time.deltaTime, miniMoveInput.normalized.y * moveSpeed * 1.2f * Time.deltaTime, 1.0f);
+            }
+        }
         // rb.linearVelocity = new Vector2(moveInput.normalized.x * moveSpeed, moveInput.normalized.y * moveSpeed);
 
-        if (moveInput.magnitude > 0.1) 
+        if (moveInput.magnitude > 0.1)
             myAnim.SetBool("IsMoving", true);
-        else 
+        else
             myAnim.SetBool("IsMoving", false);
-
     }
 }
